f1a8c3a8c2ef28f0b12422e9690165e6f89eed15
estelaV9 <esteladeveg@gmail.com>
2024-09-24 15:54:07 +0200

    Se cambia la forma en que se borra el radioBtt. Se quita el bucle do-while para que ponga otro mail, ahora solo salta una alerta y se añade una excepcion para que el usuario no ponga una letra en el descuento. EJERCICIO TERMINADO

--- Introducion/EjercicioRepasoFX/src/main/java/com/example/ejerciciorepasofx/Controller/ClienteCtrller.java
+++ Introducion/EjercicioRepasoFX/src/main/java/com/example/ejerciciorepasofx/Controller/ClienteCtrller.java
@@ -5,7 +5,6 @@ import com.example.ejerciciorepasofx.Utilities.GeneralCode;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-
 import javax.swing.*;
 import java.util.ArrayList;
 
@@ -18,7 +17,6 @@ public class ClienteCtrller {
     private TextField descuentoTxt;
     @FXML
     private TextField usuarioTxt;
-
     ArrayList<Usuario> listaClientes = new ArrayList<>();
 
     @FXML
@@ -31,33 +29,38 @@ public class ClienteCtrller {
             // GENERAR ALERTA
             GeneralCode.Alerts("ERROR", "Campos vacíos.", "¡ERROR!", "Por favor, rellene todos los datos antes de continuar.");
         } else {
-            double descuento = Double.parseDouble(descuentoTxt.getText());
-            do {
-                for (Usuario usua : listaClientes) {
-                    if (usua.getMailCl().equals(usuarioTxt.getText())) {
+            try {
+                double descuento = Double.parseDouble(descuentoTxt.getText());
+
+                for (Usuario usu : listaClientes) {
+                    if (usu.getMailCl().equals(usuarioTxt.getText())) {
                         semaforo = true;
                         break;
                     } else {
                         semaforo = false;
                     }
                 } // for para buscar ese usuario
+
                 if (semaforo) {
-                    GeneralCode.Alerts("ERROR", "Usuario ya registrado.", "¡ERROR!", "Ese usuario ya existe. Prueba con otro, por favor.");
+                    GeneralCode.Alerts("ERROR", "Usuario ya registrado.", "¡ERROR!",
+                            "Ese usuario ya existe. Prueba con otro, por favor.");
                 } // if si ese usuario si existe
-            } while (semaforo); // bucle para repetir el mail hasta que no este registrado
 
-
-            if (seleccionado.getText().equals("Si")) {
-                user = new Usuario(usuarioTxt.getText(), contraseñaTxt.getText(), descuento, true);
-                listaClientes.add(user);
-                GeneralCode.Alerts("CONFIRMATION", "Se añadió.", null,
-                        "El usuario se ha añadido correctamente.");
-            } else {
-                user = new Usuario(usuarioTxt.getText(), contraseñaTxt.getText(), descuento, false);
-                listaClientes.add(user);
-                GeneralCode.Alerts("CONFIRMATION", "Se añadió.", null,
-                        "El usuario se ha añadido correctamente.");
-            } // if para si el usuario es premium o no
+                if (seleccionado.getText().equals("Si")) {
+                    user = new Usuario(usuarioTxt.getText(), contraseñaTxt.getText(), descuento, true);
+                    listaClientes.add(user);
+                    GeneralCode.Alerts("CONFIRMATION", "Se añadió.", null,
+                            "El usuario se ha añadido correctamente.");
+                } else {
+                    user = new Usuario(usuarioTxt.getText(), contraseñaTxt.getText(), descuento, false);
+                    listaClientes.add(user);
+                    GeneralCode.Alerts("CONFIRMATION", "Se añadió.", null,
+                            "El usuario se ha añadido correctamente.");
+                } // if para si el usuario es premium o no
+            } catch (NumberFormatException e) {
+                GeneralCode.Alerts("ERROR", "Descuento no válido.", "¡ERROR!",
+                        "Por favor, introduzca un número válido para el descuento.");
+            } // EXCEPCION PARA QUE EL USUARIO NO PONGA UNA LETRA EN EL DESCUENTO
         }
     } // METODO PARA INSERTAR UN NUEVO CLIENTE
 
@@ -105,9 +108,8 @@ public class ClienteCtrller {
         contraseñaTxt.clear();
         usuarioTxt.clear();
         descuentoTxt.clear();
-        RadioButton seleccionado = (RadioButton) clientePremium.getSelectedToggle();
-        seleccionado.setSelected(false);
-    }
+        clientePremium.selectToggle(null); // DESELECCIONAR OPCION DEL TOGGLEGROUP
+    } // LIMPIAR CAMPOS
 
     @FXML
     void onSalirBtt(ActionEvent event) {

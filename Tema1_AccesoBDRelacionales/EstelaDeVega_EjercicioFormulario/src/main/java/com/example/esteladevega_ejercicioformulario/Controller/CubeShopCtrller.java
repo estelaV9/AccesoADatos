65e2bca5e52d7d27b3da746b179b4ab096c6c2b8
estelaV9 <esteladeveg@gmail.com>
2024-10-01 21:57:31 +0200

    crear la vista de tienda

---
+++ Tema1_AccesoBDRelacionales/EstelaDeVega_EjercicioFormulario/src/main/java/com/example/esteladevega_ejercicioformulario/Controller/CubeShopCtrller.java
@@ -0,0 +1,60 @@
+package com.example.esteladevega_ejercicioformulario.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+
+public class CubeShopCtrller {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Button backBtt;
+
+    @FXML
+    private Button closeBtt;
+
+    @FXML
+    private Label loginMessage;
+
+    @FXML
+    private Button newProductBtt;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private Button shopCartBtt;
+
+    @FXML
+    private Button yourProductBtt;
+
+    @FXML
+    void onBackAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCloseAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onNewProductAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onShopCartAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onYourProductAction(ActionEvent event) {
+
+    }
+}

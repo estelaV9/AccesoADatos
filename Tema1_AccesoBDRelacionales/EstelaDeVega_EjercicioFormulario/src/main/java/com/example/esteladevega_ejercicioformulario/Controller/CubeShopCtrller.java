246cce792a2dbcfcabb7d7066e2812b1ead748b7
estelaV9 <esteladeveg@gmail.com>
2024-10-01 22:20:54 +0200

    modificar la vista de tienda (añadir popup setting) y añadir funciones en su controller

--- Tema1_AccesoBDRelacionales/EstelaDeVega_EjercicioFormulario/src/main/java/com/example/esteladevega_ejercicioformulario/Controller/CubeShopCtrller.java
+++ Tema1_AccesoBDRelacionales/EstelaDeVega_EjercicioFormulario/src/main/java/com/example/esteladevega_ejercicioformulario/Controller/CubeShopCtrller.java
@@ -1,13 +1,19 @@
 package com.example.esteladevega_ejercicioformulario.Controller;
 
+import com.example.esteladevega_ejercicioformulario.Utilities.StaticCode;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
-public class CubeShopCtrller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CubeShopCtrller implements Initializable {
 
     @FXML
     private AnchorPane anchorPane;
@@ -19,6 +25,9 @@ public class CubeShopCtrller {
     private Button closeBtt;
 
     @FXML
+    private Button closeSeBtt;
+
+    @FXML
     private Label loginMessage;
 
     @FXML
@@ -28,23 +37,54 @@ public class CubeShopCtrller {
     private ScrollPane scrollPane;
 
     @FXML
+    private Button settingBtt;
+
+    @FXML
+    private Pane settingMenu;
+
+    @FXML
+    private Button settingsMenuBtt;
+
+    @FXML
     private Button shopCartBtt;
+
+    @FXML
+    private Button signOutBtt;
 
     @FXML
     private Button yourProductBtt;
 
     @FXML
     void onBackAction(ActionEvent event) {
-
-    }
+        // SE LLAMA AL METODO ESTATICO CAMBIAR VISTA POR BOTON PARA IR A LA PAGINA DEL LOGIN
+        // SE INSERTA LOS PARAMETROS: NOMBRE DEL FXML AL QUE SE QUIERE IR, UN BOTON Y
+        // EL TITULO QUE VA A TENER ESE STAGE
+        StaticCode.cambiarVistaBtt("/ui/Registration.fxml", backBtt, "Registration Page");
+    } // IR A LA PAGINA DEL LOGIN
 
     @FXML
     void onCloseAction(ActionEvent event) {
+        // SE LLAMA AL METODO ESTATICO PARA SALIR DE LA APLICACION
+        StaticCode.exitApp();
+    } // SALIR DE LA APLICACIÓN
 
+    @FXML
+    void onCloseSettingAction(ActionEvent event) {
+        settingMenu.setVisible(false);
     }
 
     @FXML
     void onNewProductAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSettingAction(ActionEvent event) {
+        settingMenu.setVisible(true);
+    }
+
+    @FXML
+    void onSettingsMenuAction(ActionEvent event) {
 
     }
 
@@ -56,5 +96,10 @@ public class CubeShopCtrller {
     @FXML
     void onYourProductAction(ActionEvent event) {
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        settingMenu.setVisible(false);
     }
 }

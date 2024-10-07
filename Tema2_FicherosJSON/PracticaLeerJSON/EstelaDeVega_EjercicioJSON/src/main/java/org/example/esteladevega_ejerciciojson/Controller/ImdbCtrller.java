545713da86b7e1bdc37743bdb3f413c26577d208
estelaV9 <esteladeveg@gmail.com>
2024-10-07 09:48:12 +0200

    añadir el fichero json y los atributos del controller

--- Tema2_FicherosJSON/PracticaLeerJSON/EstelaDeVega_EjercicioJSON/src/main/java/org/example/esteladevega_ejerciciojson/Controller/ImdbCtrller.java
+++ Tema2_FicherosJSON/PracticaLeerJSON/EstelaDeVega_EjercicioJSON/src/main/java/org/example/esteladevega_ejerciciojson/Controller/ImdbCtrller.java
@@ -1,14 +1,34 @@
 package org.example.esteladevega_ejerciciojson.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class ImdbCtrller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField dateTxt;
+
+    @FXML
+    private TextField directorTxt;
+
+    @FXML
+    private TextField genderTxt;
+
+    @FXML
+    private Button importarBtt;
+
+    @FXML
+    private ListView<?> listView;
+
+    @FXML
+    private TextField titleTxt;
+
+    @FXML
+    void onImportarAction(ActionEvent event) {
+
     }
+
 }

4f8ea95d7b1b52cf59dad6994b451786264f01f0
estelaV9 <esteladeveg@gmail.com>
2024-10-09 08:45:15 +0200

    crear el fxml para los productos propios y su controller

---
+++ Tema1_AccesoBDRelacionales/EjercicioFormularioFX/EstelaDeVega_EjercicioFormulario/src/main/java/com/example/esteladevega_ejercicioformulario/Controller/MyProductCtrller.java
@@ -0,0 +1,87 @@
+package com.example.esteladevega_ejercicioformulario.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
+
+public class MyProductCtrller {
+
+    @FXML
+    private TableView<?> CubeTable;
+
+    @FXML
+    private Button backBtt;
+
+    @FXML
+    private TableColumn<?, ?> categoryCol;
+
+    @FXML
+    private Button closeBtt;
+
+    @FXML
+    private Button closeSeBtt;
+
+    @FXML
+    private Button deleteBtt;
+
+    @FXML
+    private Label loginMessage;
+
+    @FXML
+    private Button modifyBtt;
+
+    @FXML
+    private TableColumn<?, ?> nameProductCol;
+
+    @FXML
+    private TableColumn<?, ?> priceCol;
+
+    @FXML
+    private Button settingBtt;
+
+    @FXML
+    private Pane settingMenu;
+
+    @FXML
+    private Button settingsMenuBtt;
+
+    @FXML
+    void onBackAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCloseAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCloseSettingAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onModifyAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSettingAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSettingsMenuAction(ActionEvent event) {
+
+    }
+
+}

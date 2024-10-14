6a354ef1b24341b63ae6233d3eac3592d5e2760a
estelaV9 <esteladeveg@gmail.com>
2024-10-14 11:07:09 +0200

    Crear la clase Coche con sus atributos, contructores y getter y setter

---
+++ Tema3_AccesoBDNoSQL_MongoDB/PracticaMongoDB_GestionCoches/EstelaDeVega_EjercicioCrudGestionCoche/src/main/java/org/esteladevega_ejerciciocrudgestioncoche/Model/Coche.java
@@ -0,0 +1,27 @@
+package org.esteladevega_ejerciciocrudgestioncoche.Model;
+
+public class Coche {
+    private String matricula; // ATRIBUTO PARA GUARDAR LA MATRICULA DEL COCHE
+    private String marca; // ATRIBUTO PARA GUARDAR LA MARCA DEL COCHE
+    private String modelo; // ATRIBUTO PARA GUARDAR EL MODELO DEL COCHE
+    private String tipo; // ATRIBUTO PARA GUARDAR EL TIPO DE COCHE
+
+    public Coche () {
+
+    }
+    public Coche(String matricula, String marca, String modelo, String tipo) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tipo = tipo;
+    }
+
+    public String getMatricula() { return matricula; }
+    public void setMatricula(String matricula) { this.matricula = matricula; }
+    public String getMarca() { return marca; }
+    public void setMarca(String marca) { this.marca = marca; }
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+}

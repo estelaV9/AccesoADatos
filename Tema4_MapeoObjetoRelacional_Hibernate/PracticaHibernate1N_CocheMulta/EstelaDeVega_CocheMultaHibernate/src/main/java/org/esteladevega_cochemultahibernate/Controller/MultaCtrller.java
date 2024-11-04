44dc74ae030f670da8b63a159ab16a8f493b841b
estelaV9 <esteladeveg@gmail.com>
2024-11-04 11:05:54 +0100

    crear parte visual de multas (sin terminar) y poner en su controller sus atributos y metodos

---
+++ Tema4_MapeoObjetoRelacional_Hibernate/PracticaHibernate1N_CocheMulta/EstelaDeVega_CocheMultaHibernate/src/main/java/org/esteladevega_cochemultahibernate/Controller/MultaCtrller.java
@@ -0,0 +1,78 @@
+package org.esteladevega_cochemultahibernate.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class MultaCtrller {
+
+    @FXML
+    private Button actualizarBtt;
+
+    @FXML
+    private Button borrarBtt;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private Button exitBtt;
+
+    @FXML
+    private TableColumn<?, ?> fechaCol;
+
+    @FXML
+    private TableColumn<?, ?> idCol;
+
+    @FXML
+    private TextField idMultaTF;
+
+    @FXML
+    private Button insertarBtt;
+
+    @FXML
+    private Button limpiarBtt;
+
+    @FXML
+    private TextField matriculaTF;
+
+    @FXML
+    private TableView<?> multaTable;
+
+    @FXML
+    private TableColumn<?, ?> precioCol;
+
+    @FXML
+    private TextField precioTF;
+
+    @FXML
+    void onActualizarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBorrarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickedTable(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onExitAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onInsertarAction(ActionEvent event) {
+
+    }
+
+}

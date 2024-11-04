3ab4fb78f651f27ca866d8d21db08b38501ddbe0
estelaV9 <esteladeveg@gmail.com>
2024-11-04 10:02:28 +0100

    crear parte visual de coches con sus atributos y metodos en el controller

--- Tema4_MapeoObjetoRelacional_Hibernate/PracticaHibernate1N_CocheMulta/EstelaDeVega_CocheMultaHibernate/src/main/java/org/esteladevega_cochemultahibernate/Controller/CocheCtrller.java
+++ Tema4_MapeoObjetoRelacional_Hibernate/PracticaHibernate1N_CocheMulta/EstelaDeVega_CocheMultaHibernate/src/main/java/org/esteladevega_cochemultahibernate/Controller/CocheCtrller.java
@@ -1,14 +1,92 @@
 package org.esteladevega_cochemultahibernate.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class CocheCtrller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button cancelarBtt;
+
+    @FXML
+    private TableView<?> cochesTable;
+
+    @FXML
+    private Button eliminarBtt;
+
+    @FXML
+    private Button exitBtt;
+
+    @FXML
+    private TableColumn<?, ?> idCol;
+
+    @FXML
+    private TableColumn<?, ?> marcaCol;
+
+    @FXML
+    private TextField marcaTxt;
+
+    @FXML
+    private TableColumn<?, ?> matriculaCol;
+
+    @FXML
+    private TextField matriculaTxt;
+
+    @FXML
+    private TableColumn<?, ?> modeloCol;
+
+    @FXML
+    private TextField modeloTxt;
+
+    @FXML
+    private Button modificarBtt;
+
+    @FXML
+    private Button nuevoBtt;
+
+    @FXML
+    private Button nuevoBtt1;
+
+    @FXML
+    private TableColumn<?, ?> tipoCol;
+
+    @FXML
+    private ComboBox<?> tipoComboBox;
+
+    @FXML
+    void onCancelarAction(ActionEvent event) {
+
     }
+
+    @FXML
+    void onClickedTable(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onEliminarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onExitAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onModificarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onNuevoCocheAction(ActionEvent event) {
+
+    }
+
 }

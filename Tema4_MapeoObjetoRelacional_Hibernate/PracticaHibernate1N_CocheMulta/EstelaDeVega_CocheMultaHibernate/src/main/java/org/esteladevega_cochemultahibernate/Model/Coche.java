3c8a3cf51748d9013d38baa51ee1efaeb4580311
estelaV9 <esteladeveg@gmail.com>
2024-11-06 09:38:44 +0100

    crear la clase coche con sus atributos

---
+++ Tema4_MapeoObjetoRelacional_Hibernate/PracticaHibernate1N_CocheMulta/EstelaDeVega_CocheMultaHibernate/src/main/java/org/esteladevega_cochemultahibernate/Model/Coche.java
@@ -0,0 +1,79 @@
+package org.esteladevega_cochemultahibernate.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "coches")
+public class Coche {
+    @Id
+    @Column(name = "id")
+    private int idCoche;
+
+    @Column(name = "matricula")
+    private String matricula;
+
+    @Column(name = "marca")
+    private String marca;
+
+    @Column(name = "modelo")
+    private String modelo;
+
+    @Column(name = "tipo")
+    private String tipo;
+
+    public Coche(String matricula, String marca, String modelo, String tipo) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tipo = tipo;
+    }
+
+    public Coche(int idCoche, String matricula, String marca, String modelo, String tipo) {
+        this.idCoche = idCoche;
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tipo = tipo;
+    }
+
+
+    public int getIdCoche() {
+        return idCoche;
+    }
+
+    public void setIdCoche(int idCoche) {
+        this.idCoche = idCoche;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}

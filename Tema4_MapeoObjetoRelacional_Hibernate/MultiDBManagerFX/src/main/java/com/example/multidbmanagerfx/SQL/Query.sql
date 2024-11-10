464985407d3badb74630d30cf35022106e483493
estelaV9 <esteladeveg@gmail.com>
2024-11-10 12:58:10 +0100

    añadir la tabla multas

--- Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/SQL/Query.sql
+++ Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/SQL/Query.sql
@@ -15,3 +15,22 @@ INSERT INTO coches (matricula, marca, modelo, tipo)
 VALUES ('1234AAA', 'Audi', 'Q5', 'SUV'),
  ('2345BBB', 'Ford', 'S-mMax', 'SUV'),
  ('9876CCC', 'Renault', 'Clio', 'SUV');
+
+
+
+CREATE TABLE multas (
+  id_multa INTEGER NOT NULL AUTO_INCREMENT,
+  precio DOUBLE NOT NULL,
+  fecha DATE DEFAULT NULL,
+  matricula VARCHAR(7) NOT NULL,
+  PRIMARY KEY (id_multa)
+  ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
+
+INSERT INTO multas (matricula, precio, fecha)
+VALUES ('1234AAA', 200, '2023-12-12'),
+       ('1234AAA', 700, '2023-12-13'),
+       ('1234AAA', 50, '2023-12-14'),
+       ('2345BBB', 250, '2024-12-01'),
+        ('9876CCC', 100, '2024-11-21');
+
+SELECT * FROM coches;

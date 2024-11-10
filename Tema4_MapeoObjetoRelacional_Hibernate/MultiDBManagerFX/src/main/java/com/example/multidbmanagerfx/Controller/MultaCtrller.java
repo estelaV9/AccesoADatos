32164079dd18186bec2613c67bccce8d71da039b
estelaV9 <esteladeveg@gmail.com>
2024-11-10 13:37:18 +0100

    añadir un boton de back y crear el controlador de multa con sus metodos

---
+++ Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/Controller/MultaCtrller.java
@@ -0,0 +1,73 @@
+package com.example.multidbmanagerfx.Controller;
+
+import com.example.multidbmanagerfx.Utilities.StaticCode;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class MultaCtrller {
+    @FXML
+    private Button actualizarBtt;
+    @FXML
+    private Button backBtt;
+    @FXML
+    private Button borrarBtt;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private Button exitBtt;
+    @FXML
+    private TableColumn<?, ?> fechaCol;
+    @FXML
+    private TableColumn<?, ?> idCol;
+    @FXML
+    private TextField idMultaTF;
+    @FXML
+    private Button insertarBtt;
+    @FXML
+    private Button limpiarBtt;
+    @FXML
+    private TextField matriculaTF;
+    @FXML
+    private TableView<?> multaTable;
+    @FXML
+    private TableColumn<?, ?> precioCol;
+    @FXML
+    private TextField precioTF;
+
+    @FXML
+    void onActualizarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBackAction(ActionEvent event) {
+        StaticCode.changeViewBtt("/ui/Coche.fxml", backBtt, "Gestión Coches y Multas");
+    }
+
+    @FXML
+    void onBorrarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickedTable(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onExitAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onInsertarAction(ActionEvent event) {
+
+    }
+
+}

1ddbc0af5d8f952578984894e6e13de7da14c8b9
estelaV9 <esteladeveg@gmail.com>
2024-11-10 19:40:27 +0100

    añadir el metodo de executeAction y modificar el codigo de multasContrller

--- Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/Controller/MultaCtrller.java
+++ Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/Controller/MultaCtrller.java
@@ -1,19 +1,17 @@
 package com.example.multidbmanagerfx.Controller;
 
-import com.example.multidbmanagerfx.DAO.MultaDAO;
+import com.example.multidbmanagerfx.DAO.Hibernate_MultasDAO;
+import com.example.multidbmanagerfx.DAO.MySQL_MultaDAO;
 import com.example.multidbmanagerfx.Model.Coche;
 import com.example.multidbmanagerfx.Model.Multa;
 import com.example.multidbmanagerfx.Utilities.StaticCode;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+
 import java.net.URL;
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -49,19 +47,62 @@ public class MultaCtrller implements Initializable {
     @FXML
     private TextField precioTF;
 
-    MultaDAO multaDAO = new MultaDAO();
+    MySQL_MultaDAO multaDAO = new MySQL_MultaDAO();
 
     String numberPlate; // VARIABLE PARA GUARDAR LA MATRICULA DEL COCHE SELECCIONADO
+    static String selectedGestor;
 
-    public void displayCoche(Coche coche) {
+    private boolean executeAction(String action, Multa multa, Multa modifyFineHibernate) {
+        // RESULTADO POR DEFECTO (INICIALIZADO EN FALSO)
+        boolean result = false;
+
+        // EVALUAR EL GESTOR SELECCIONADO PARA EJECUTAR LA ACCIÓN CORRESPONDIENTE
+        switch (selectedGestor) {
+            case "MySQL" -> {
+                if ("eliminar".equals(action)) {
+                    result = multaDAO.deleteFine(multa.getCoche().getMatricula()); // Eliminar multa en MySQL
+                } else if ("modificar".equals(action)) {
+                    result = multaDAO.modifyFine(multa, multa.getIdMulta()); // Modificar multa en MySQL
+                } else if ("insertar".equals(action)) {
+                    result = multaDAO.insertFine(multa); // Insertar multa en MySQL
+                }
+            }
+
+            case "Hibernate" -> {
+                Hibernate_MultasDAO hibernateMultaDAO = new Hibernate_MultasDAO();
+
+                if ("eliminar".equals(action)) {
+                    result = hibernateMultaDAO.deleteFine(multa.getCoche().getMatricula()); // Eliminar multa en Hibernate
+                } else if ("modificar".equals(action)) {
+                    result = hibernateMultaDAO.modifyFine(modifyFineHibernate, multa.getIdMulta()); // Modificar multa en Hibernate
+                } else if ("insertar".equals(action)) {
+                    result = hibernateMultaDAO.insertFine(multa); // Insertar multa en Hibernate
+                }
+            }
+
+            default -> {
+                // Si no se selecciona un gestor válido, mostrar mensaje de error
+                StaticCode.Alerts("ERROR", "Gestor desconocido", "¡ERROR!", "No se ha seleccionado un gestor válido.");
+            }
+        }
+
+        // Devolver el resultado de la acción (TRUE si se ejecutó correctamente, FALSE en caso contrario)
+        return result;
+    }
+
+
+    public void displayCoche(Coche coche, ToggleGroup dbGroup) {
         matriculaTF.setText(coche.getMatricula()); // SETTEA LA MATRICULA DEL COCHE SELECI
         matriculaTF.setEditable(false);
         numberPlate = coche.getMatricula(); // ASIGNA LA MATRICULA A LA VARIABLE GLOBAL
+        // OBTENER GESTOR SELECCIONADO
+        selectedGestor = ((RadioButton) dbGroup.getSelectedToggle()).getText();
+
         refreshTable(); // LLAMA A refreshTable() DESPUES DE HABER ASIGNADO LA MATRICULA
     } // METODO QUE GUARDA LOS DATOS DEL COCHE SELECCIONADO CUANDO SE CAMBIA DE ESCENA
 
     @FXML
-    void onLimpiarAction(){
+    void onLimpiarAction() {
         idMultaTF.clear();
         datePicker.setValue(null);
         precioTF.clear();
@@ -74,16 +115,17 @@ public class MultaCtrller implements Initializable {
             StaticCode.Alerts("ERROR", "Selecciona una multa", "¡ERROR!",
                     "Por favor, seleccione una multa para eliminar");
         } else {
-            Multa fine = new Multa(Integer.parseInt(idMultaTF.getText()), Double.parseDouble(precioTF.getText()), LocalDate.parse(datePicker.getValue().toString()), matriculaTF.getText());
-            if(multaDAO.modifyFine(fine, fineSelected.getIdMulta())){
+            Coche coche = new Coche(matriculaTF.getText());
+            Multa fine = new Multa(Integer.parseInt(idMultaTF.getText()), Double.parseDouble(precioTF.getText()), LocalDate.parse(datePicker.getValue().toString()), coche);
+            if (executeAction("modificar", fine, fineSelected)) {
                 // SI SE MODIFICO LA MULTA CORRECTAMENTE, MOSTRAR UN MENSAJE DE EXITO
                 StaticCode.Alerts("INFORMATION", "Modificación de multa",
-                        "Modificación exitosa","Se ha modificado la multa correctamente.");
+                        "Modificación exitosa", "Se ha modificado la multa correctamente.");
                 refreshTable(); // REFRESCAR LOS DATOS
             } else {
                 // SI NO SE MODIFICO, SE MUESTRA UN ERROR
                 StaticCode.Alerts("ERROR", "Modificación de multa",
-                        "Modificación fallida","No se ha podido modificar la multa.");
+                        "Modificación fallida", "No se ha podido modificar la multa.");
             } // MODIFICAR LA MULTA
         } // VALIDAR SI HA SELECCIONADO UNA MULTA
     } // BOTON PARA MODIFICAR UNA MULTA DE UNA MATRICULA SELECCIONADA
@@ -100,15 +142,15 @@ public class MultaCtrller implements Initializable {
             StaticCode.Alerts("ERROR", "Selecciona una multa", "¡ERROR!",
                     "Por favor, seleccione una multa para eliminar");
         } else {
-            if(multaDAO.deleteFine(fineSelected.getMatricula())){
+            if (executeAction("eliminar", fineSelected, null)) {
                 // SI SE ELIMINO LA MULTA CORRECTAMENTE, MOSTRAR UN MENSAJE DE EXITO
                 StaticCode.Alerts("INFORMATION", "Eliminación de multa",
-                        "Eliminación exitosa","Se ha eliminado la multa correctamente.");
+                        "Eliminación exitosa", "Se ha eliminado la multa correctamente.");
                 refreshTable(); // REFRESCAR LOS DATOS
             } else {
                 // SI NO SE ELIMINO, SE MUESTRA UN ERROR
                 StaticCode.Alerts("ERROR", "Eliminación de multa",
-                        "Eliminación fallida","No se ha podido eliminar la multa.");
+                        "Eliminación fallida", "No se ha podido eliminar la multa.");
             } // BORRAR LA MULTA
         } // VALIDAR SI HA SELECCIONADO UNA MULTA
     } // BOTON PARA ELIMINAR UNA MULTA DE UNA MATRICULA SELECCIONADA
@@ -116,7 +158,7 @@ public class MultaCtrller implements Initializable {
     @FXML
     void onClickedTable(MouseEvent event) {
         Multa fineSelected = multaTable.getSelectionModel().getSelectedItem(); // OBJETO CON LA MULTA SELECCIONADA
-        if(fineSelected != null){
+        if (fineSelected != null) {
             idMultaTF.setText(String.valueOf(fineSelected.getIdMulta()));
             precioTF.setText(String.valueOf(fineSelected.getPrecio()));
             datePicker.setValue(fineSelected.getFecha());
@@ -130,8 +172,9 @@ public class MultaCtrller implements Initializable {
 
     @FXML
     void onInsertarAction(ActionEvent event) {
-        Multa fine = new Multa(Integer.parseInt(idMultaTF.getText()), Double.parseDouble(precioTF.getText()), LocalDate.parse(datePicker.getValue().toString()), matriculaTF.getText());
-        if(multaDAO.insertFine(fine)){
+        Coche coche = new Coche(matriculaTF.getText());
+        Multa fine = new Multa(Integer.parseInt(idMultaTF.getText()), Double.parseDouble(precioTF.getText()), LocalDate.parse(datePicker.getValue().toString()), coche);
+        if (executeAction("insertar", fine, null)) {
             // SI SE INSERTO LA MULTA CORRECTAMENTE, MOSTRAR UN MENSAJE DE EXITO
             StaticCode.Alerts("INFORMATION", "Creación de multa", "Creación exitosa",
                     "Se ha creado la multa correctamente.");

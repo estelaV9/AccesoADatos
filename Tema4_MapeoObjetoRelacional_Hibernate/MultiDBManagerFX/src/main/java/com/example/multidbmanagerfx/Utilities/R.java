39e3d4f13f2db238d496cada10e4a364b829739b
estelaV9 <esteladeveg@gmail.com>
2024-11-10 13:35:56 +0100

    añadir el metodo de getStyleSheet a la clase R para cambiar al stage con el estilo

--- Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/Utilities/R.java
+++ Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/Utilities/R.java
@@ -8,6 +8,10 @@ public class R {
     public static InputStream getImage(String name) {
         return Thread.currentThread().getContextClassLoader().getResourceAsStream("images" + File.separator + name);
     }
+    public static URL getStyleSheet(String name) {
+        return Thread.currentThread().getContextClassLoader().getResource("ui" + File.separator + name);
+    }
+
     public static InputStream getProperties(String name) {
         return Thread.currentThread().getContextClassLoader().getResourceAsStream("configuration" + File.separator + name);
     }

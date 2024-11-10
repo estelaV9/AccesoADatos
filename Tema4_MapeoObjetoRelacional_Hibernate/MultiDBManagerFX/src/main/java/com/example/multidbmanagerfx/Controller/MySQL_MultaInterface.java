756bf0e596530faf187d81396b292101e0b17125
estelaV9 <esteladeveg@gmail.com>
2024-11-10 15:55:17 +0100

    crear el interface de multa

---
+++ Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/Controller/MySQL_MultaInterface.java
@@ -0,0 +1,9 @@
+package com.example.multidbmanagerfx.Controller;
+
+import com.example.multidbmanagerfx.Model.Multa;
+import javafx.collections.ObservableList;
+
+public interface MySQL_MultaInterface {
+    ObservableList<Multa> listOfFines(String carNumberPlate); // LISTAR MULTAS
+    boolean insertFine(Multa multa); // INSERTAR MULTA
+}

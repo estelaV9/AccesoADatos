96a5017b00c50ae1ffe74009e8227dccd28f1eef
estelaV9 <esteladeveg@gmail.com>
2024-11-10 09:55:21 +0100

    cambiar el nombre del archivo properties, retornar el atributo mysqlConnection y añadir el metodo desconectar

--- Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/Connection/MySQL_ConnectionDB.java
+++ Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/Connection/MySQL_ConnectionDB.java
@@ -3,6 +3,7 @@ package com.example.multidbmanagerfx.Connection;
 import com.example.multidbmanagerfx.Utilities.R;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class MySQL_ConnectionDB {
@@ -11,22 +12,27 @@ public class MySQL_ConnectionDB {
     public static Connection conectar() {
         try {
             Properties properties = new Properties();
-            properties.load(R.getProperties("database.properties"));
+            properties.load(R.getProperties("MySQL_Database.properties"));
 
             String host = properties.getProperty("host");
             String port = properties.getProperty("port");
-            String name = properties.getProperty("name");
+            String name = properties.getProperty("dbname");
             String username = properties.getProperty("username");
             String password = properties.getProperty("password");
 
-            return DriverManager.getConnection(
+            mysqlConnection = DriverManager.getConnection(
                     "jdbc:mysql://" + host + ":" + port + "/" + name + "?serverTimezone=UTC",
                     username,
                     password
             );
+            return mysqlConnection;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
-    }
+    } // METODO PARA CONECTAR CON LA BASE DE DATOS DONDE CARGARA LOS DATOS DEL ARCHIVO CONFIGURATION
+
+    public static void desconectar() throws SQLException {
+        mysqlConnection.close();
+    } // METODO PARA DESCONECTAR UNA APP A LA DATABASE
 }

fc66332779d7c297d1d34389bfec707c13c97a18
estelaV9 <esteladeveg@gmail.com>
2024-11-10 22:51:20 +0100

    cambiar el metodo conectar a mongo

--- Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/Connection/MongoDB_ConnectionDB.java
+++ Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/Connection/MongoDB_ConnectionDB.java
@@ -1,31 +1,45 @@
 package com.example.multidbmanagerfx.Connection;
 
+import com.example.multidbmanagerfx.Utilities.StaticCode;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 import com.example.multidbmanagerfx.Utilities.R;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class MongoDB_ConnectionDB {
     static MongoClient mongoClient;
     static MongoDatabase database;
-    static {
-        try {
-            Properties configurationDB = new Properties(); // CREAR OBJETO TIPO PROPERTIES
-            configurationDB.load(R.getProperties("Mongo_Database.properties")); // LEER EL ARCHIVO PROPERTIES Y COGER LOS DATOS
-            String host = configurationDB.getProperty("host");
-            String user = configurationDB.getProperty("user");
-            String pass = configurationDB.getProperty("password");
-            String port = configurationDB.getProperty("port");
-            String source = configurationDB.getProperty("source");
+    public static MongoClient conectar() {
+        Properties properties = new Properties();
+        String host = "", port = "", name = "", username = "", password = "";
 
-            mongoClient = new MongoClient(new MongoClientURI("mongodb://"+user+":"+pass+"@"+host+":"+port+"/?authSource="+source)); // CONECTAR A LA BASE DE DATOS
-            database = mongoClient.getDatabase("CarManagementDB");
+        try {
+            properties.load(new FileInputStream("src/main/resources/configuration/Mongo_Database.properties"));
+            host = String.valueOf(properties.get("host"));
+            port = String.valueOf(properties.get("port"));
+            name = String.valueOf(properties.get("name"));
+            username = String.valueOf(properties.get("username"));
+            password = String.valueOf(properties.get("password"));
+            return mongoClient = new MongoClient(new MongoClientURI("mongodb://" + username + ":" + password + "@" + host + ":" + port + "/?authSource=admin"));
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            StaticCode.Alerts("ERROR", "Error de conexión",
+                    "Archivo de configuración no encontrado",
+                    "No se ha encontrado el archivo database.properties: " + e.getMessage());
+            return null;
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            // TODO Auto-generated catch block
+            StaticCode.Alerts("ERROR", "Error de conexión",
+                    "Error de E/S", "Hubo un problema al leer el archivo de configuración: " + e.getMessage());
+            e.printStackTrace();
+            return null;
         }
-    }
+    } // METODO PARA CONECTAR UNA APP A LA DATABASE DE MONGODB
     public static void closeDataBase(){
         mongoClient.close();
     }

1c37163da84684e4857e43ca8787a056f21d2a42
estelaV9 <esteladeveg@gmail.com>
2024-11-10 19:37:34 +0100

    cambiar de nombre a los interfaces de mysql

--- Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/DAO/CocheInterface.java
+++ Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/DAO/CocheInterface.java
@@ -3,7 +3,7 @@ package com.example.multidbmanagerfx.DAO;
 import com.example.multidbmanagerfx.Model.Coche;
 import javafx.collections.ObservableList;
 
-public interface MySQL_CocheInterface {
+public interface CocheInterface {
     boolean insertCar(Coche coche); // INSERTAR COCHE
     boolean modifyCar(Coche newCar, String carNumberPlate); // MODIFICAR COCHE
     boolean deleteCar(String carNumberPlate); // ELIMINAR COCHE
--- Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/DAO/MultaInterface.java
+++ Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/DAO/MultaInterface.java
@@ -3,7 +3,7 @@ package com.example.multidbmanagerfx.DAO;
 import com.example.multidbmanagerfx.Model.Multa;
 import javafx.collections.ObservableList;
 
-public interface MySQL_MultaInterface {
+public interface MultaInterface {
     ObservableList<Multa> listOfFines(String carNumberPlate); // LISTAR MULTAS
     boolean insertFine(Multa multa); // INSERTAR MULTA
     boolean deleteFine(String carNumberPlate); // ELIMINAR MULTA

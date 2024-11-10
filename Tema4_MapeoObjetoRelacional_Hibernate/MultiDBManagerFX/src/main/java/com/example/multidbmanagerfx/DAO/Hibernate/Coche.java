05152a3062006f0d3847f596ff63ba49d272495a
estelaV9 <esteladeveg@gmail.com>
2024-11-10 16:23:39 +0100

    crear las clases coche y multas de hibernate

---
+++ Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/DAO/Hibernate/Coche.java
@@ -0,0 +1,78 @@
+package com.example.multidbmanagerfx.DAO.Hibernate;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "coches")
+public class Coche {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int idCoche; // ATRIBUTO PARA GUARDAR EL ID DEL COCHE
+
+    @Column(name = "matricula")
+    private String matricula; // ATRIBUTO PARA GUARDAR LA MATRICULA DEL COCHE
+
+    @Column(name = "marca")
+    private String marca; // ATRIBUTO PARA GUARDAR LA MARCA DEL COCHE
+
+    @Column(name = "modelo")
+    private String modelo; // ATRIBUTO PARA GUARDAR EL MODELO DEL COCHE
+
+    @Column(name = "tipo")
+    private String tipo; // ATRIBUTO PARA GUARDAR EL TIPO DE COCHE
+
+    @OneToMany(mappedBy = "coche", cascade = CascadeType.ALL) // NOMBRE DE LA CLASE SIN MAYUSCULAS
+    private List<Multa> listaMultas;
+
+    public Coche(String matricula, String marca, String modelo, String tipo) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tipo = tipo;
+    }
+
+    public Coche(int idCoche, String matricula, String marca, String modelo, String tipo) {
+        this.idCoche = idCoche;
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tipo = tipo;
+    }
+
+    public int getIdCoche() {
+        return idCoche;
+    }
+    public void setIdCoche(int idCoche) {
+        this.idCoche = idCoche;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ Tema4_MapeoObjetoRelacional_Hibernate/MultiDBManagerFX/src/main/java/com/example/multidbmanagerfx/DAO/Hibernate/Multa.java
@@ -0,0 +1,69 @@
+package com.example.multidbmanagerfx.DAO.Hibernate;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "multas")
+public class Multa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_multa")
+    private int idCoche;
+
+    @Column(name = "precio")
+    private double precio;
+
+    @Column(name = "fecha")
+    private LocalDate fecha;
+
+    @Column(name = "matricula")
+    private String modelo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_coche", referencedColumnName = "id")
+    private Coche coche;
+
+    public Multa(Coche coche, String modelo, LocalDate fecha, double precio, int idCoche) {
+        this.coche = coche;
+        this.modelo = modelo;
+        this.fecha = fecha;
+        this.precio = precio;
+        this.idCoche = idCoche;
+    }
+
+    public int getIdCoche() {
+        return idCoche;
+    }
+    public void setIdCoche(int idCoche) {
+        this.idCoche = idCoche;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Coche getCoche() {
+        return coche;
+    }
+    public void setCoche(Coche coche) {
+        this.coche = coche;
+    }
+}

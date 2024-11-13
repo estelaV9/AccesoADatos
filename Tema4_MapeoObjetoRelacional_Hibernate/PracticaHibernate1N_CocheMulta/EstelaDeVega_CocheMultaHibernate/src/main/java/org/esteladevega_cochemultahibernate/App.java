36da02aec483c3397fbb1154227b8e344739db31
estelaV9 <esteladeveg@gmail.com>
2024-11-13 08:58:10 +0100

    subir una imagen del logo y añadirle al main

--- Tema4_MapeoObjetoRelacional_Hibernate/PracticaHibernate1N_CocheMulta/EstelaDeVega_CocheMultaHibernate/src/main/java/org/esteladevega_cochemultahibernate/App.java
+++ Tema4_MapeoObjetoRelacional_Hibernate/PracticaHibernate1N_CocheMulta/EstelaDeVega_CocheMultaHibernate/src/main/java/org/esteladevega_cochemultahibernate/App.java
@@ -3,6 +3,7 @@ package org.esteladevega_cochemultahibernate;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,8 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/ui/Coche.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Multas");
+        // AÑADIR IMAGEN EN EL STAGE DEL LOGO DE LA APLICACION
+        stage.getIcons().add(new Image(this.getClass().getResource("/ui/Image/logoGestionCoches.png").toString()));
         stage.setScene(scene);
         stage.show();
     }

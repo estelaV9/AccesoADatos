befcd4236b30d854097c7c649f68a70fe262a691
estelaV9 <esteladeveg@gmail.com>
2024-11-18 10:46:16 +0100

    crear la tabla para relacionar producto y proveedor con un atributo propio de la tabla

---
+++ Tema4_MapeoObjetoRelacional_Hibernate/HibernateNaN/HibernateRelacionNaN/src/main/java/model/Producto_Proveedor.java
@@ -0,0 +1,55 @@
+package model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "producto_proveedor")
+public class Producto_Proveedor implements Serializable {
+    // LA PRIMARY KEY ESTA CONFORMADA POR EL ID DE PROVEEDORES Y EL ID DE PRODUCTO
+    @Id
+    @ManyToOne // RELACION INVERSA
+    @JoinColumn(name = "idproveedor", referencedColumnName = "idproveedor")
+    private Proveedores proveedores; // SE TRABAJA MAS FACIL CON OBJETOS
+
+    @Id
+    @ManyToOne // RELACION INVERSA
+    @JoinColumn(name = "idproducto", referencedColumnName = "idproducto")
+    private Productos productos; // SE TRABAJA MAS FACIL CON OBJETOS
+
+    @Column(name = "cantidad")
+    private int cantidad;
+
+    public Producto_Proveedor() {
+    }
+
+    public Producto_Proveedor(Proveedores proveedores, Productos productos, int cantidad) {
+        this.proveedores = proveedores;
+        this.productos = productos;
+        this.cantidad = cantidad;
+    }
+
+    public Proveedores getProveedores() {
+        return proveedores;
+    }
+
+    public void setProveedores(Proveedores proveedores) {
+        this.proveedores = proveedores;
+    }
+
+    public Productos getProductos() {
+        return productos;
+    }
+
+    public void setProductos(Productos productos) {
+        this.productos = productos;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}

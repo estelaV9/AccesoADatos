56e8f2e196370a172a0e9ac99b39055cb1952b04
estelaV9 <esteladeveg@gmail.com>
2025-01-08 22:28:13 +0100

    crear una query personalizada para buscar un hotel por localidad

--- Tema5_API/Practica_APIyOpenAPI_Swagger/EstelaDeVega_PracticaAPI/src/main/java/com/example/esteladevega_practicaapi/repository/HotelRepository.java
+++ Tema5_API/Practica_APIyOpenAPI_Swagger/EstelaDeVega_PracticaAPI/src/main/java/com/example/esteladevega_practicaapi/repository/HotelRepository.java
@@ -2,7 +2,15 @@ package com.example.esteladevega_practicaapi.repository;
 
 import com.example.esteladevega_practicaapi.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 // EL REPOSITORY SIRVE PARA HACER CONSULTAS PERSONALIZADAS
 public interface HotelRepository extends JpaRepository<Hotel, Integer> {
+    @Query("SELECT h FROM Hotel h WHERE h.localidad = :localidad")
+    List<Hotel> buscarHotelLocalidad(
+            @Param("localidad") String localidad
+    ); // QUERY PERSONALIZADA QUE DEVUELVE UNA LISTA DE HOTELES POR LOCALIDAD
 }

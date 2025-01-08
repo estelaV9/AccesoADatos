3640c63f06053b96c9836b88ce61138470964341
estelaV9 <esteladeveg@gmail.com>
2025-01-08 09:45:37 +0100

    crear la practica obligatoria API y OPEN API(Swagger)

---
+++ Tema5_API/Practica_APIyOpenAPI_Swagger/EstelaDeVega_PracticaAPI/src/test/java/com/example/esteladevega_practicaapi/EstelaDeVegaPracticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esteladevega_practicaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstelaDeVegaPracticaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

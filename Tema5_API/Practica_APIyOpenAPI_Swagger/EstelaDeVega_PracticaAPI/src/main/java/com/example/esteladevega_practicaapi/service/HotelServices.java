d4feb99789d0ab0313bcf6af6b8523bef78240d2
estelaV9 <esteladeveg@gmail.com>
2025-01-08 11:17:59 +0100

    crear la clase para el servicio del controlador del hotes con los metodos para guardar y buscar

---
+++ Tema5_API/Practica_APIyOpenAPI_Swagger/EstelaDeVega_PracticaAPI/src/main/java/com/example/esteladevega_practicaapi/service/HotelServices.java
@@ -0,0 +1,24 @@
+package com.example.esteladevega_practicaapi.service;
+
+import com.example.esteladevega_practicaapi.model.Hotel;
+import com.example.esteladevega_practicaapi.repository.HotelRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class HotelServices {
+    private final HotelRepository hotelRepository; // REPOSITORIO HOTEL
+
+    public HotelServices(HotelRepository hotelRepository) {
+        this.hotelRepository = hotelRepository;
+    } // CONSTRUCTOR PARA AÑADIR EL REPOSITORIO HOTEL EN EL SERVICIO
+
+    public <S extends Hotel> S save(S entity) {
+        return hotelRepository.save(entity);
+    } // METODO PARA GUARDAR UNA ENTIDAD DE TIPO HOTEL
+
+    // MÉTODO PARA OBTENER TODAS LAS ENTIDADES DE TIPO HOTEL
+    public List<Hotel> findAll() {
+        return hotelRepository.findAll();
+    } // METODO QUE RETORNA TODOS LOS HOTELES
+}

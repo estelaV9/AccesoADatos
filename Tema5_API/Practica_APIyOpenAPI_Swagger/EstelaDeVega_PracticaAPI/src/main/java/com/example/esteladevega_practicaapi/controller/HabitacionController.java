47b98332c0796c1a068b50aa82dc22d60930c62c
estelaV9 <esteladeveg@gmail.com>
2025-01-10 22:50:13 +0100

    añadir los metodos para listar todas las habitaciones y eliminar por hotel

--- Tema5_API/Practica_APIyOpenAPI_Swagger/EstelaDeVega_PracticaAPI/src/main/java/com/example/esteladevega_practicaapi/controller/HabitacionController.java
+++ Tema5_API/Practica_APIyOpenAPI_Swagger/EstelaDeVega_PracticaAPI/src/main/java/com/example/esteladevega_practicaapi/controller/HabitacionController.java
@@ -1,10 +1,14 @@
 package com.example.esteladevega_practicaapi.controller;
 
 import com.example.esteladevega_practicaapi.model.Habitacion;
+import com.example.esteladevega_practicaapi.model.Hotel;
 import com.example.esteladevega_practicaapi.service.HabitacionServices;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController // CONTROLADOR REST
 @RequestMapping("/api/habitacion") // RUTA BASE DE LAS PETICIONES DEL CONTROLADOR DE HABITACION
@@ -15,10 +19,29 @@ public class HabitacionController {
         this.habitacionServices = habitacionServices;
     } // CONSTRUCTOR PARA AÑADIR EL SERVICIO HOTEL EN EL CONTROLADOR
 
+    @GetMapping("/all")
+    public List<Habitacion> getAllHabitaciones() {
+        try {
+            // SE LLAMA AL SERVICIO PARA OBTENER TODAS LAS HABITACIONES
+            return habitacionServices.findAll();
+        } catch (Exception e) {
+            // SE MANEJA UNA EXCEPCION Y SE LANZA UN BAD REQUEST SI FALLA
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Error al listar todas las habitacioens ", e);
+        }
+    } // METODO PARA LISTAR TODAS LAS HABITACIONES, PETICION HTTP GET EN LA RUTA "/all"
+
     @PostMapping("/save")
     public ResponseEntity<?> createHabitacion(@RequestBody Habitacion habitacion) {
         habitacionServices.save(habitacion); // SE GUARDA LA HABITACION
         // SE RETORNA UNA RESPUESTA HTTP CON EL ESTADO CREADO
         return new ResponseEntity<>(HttpStatus.CREATED);
     } // METODO PARA CREAR UNA HABITACION, MANEJANDO LA PETICION HTTP POST EN LA RUTA "/save"
+
+    @DeleteMapping("/delete/{idHabitacion}")
+    public ResponseEntity<?> deleteHabitacionByHotel(@PathVariable int idHabitacion){
+        habitacionServices.deleteById(idHabitacion); // SE ELIMNA LA HABITACION
+        // SE RETORNA UNA RESPUESTA HTTP CON EL ESTADO CREADO
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    } // METODO PARA ELIMINAR UNA HABITACION DETERMINADA DE UN HOTEL
 }
--- Tema5_API/Practica_APIyOpenAPI_Swagger/EstelaDeVega_PracticaAPI/src/main/java/com/example/esteladevega_practicaapi/service/HabitacionServices.java
+++ Tema5_API/Practica_APIyOpenAPI_Swagger/EstelaDeVega_PracticaAPI/src/main/java/com/example/esteladevega_practicaapi/service/HabitacionServices.java
@@ -5,6 +5,8 @@ import com.example.esteladevega_practicaapi.model.Hotel;
 import com.example.esteladevega_practicaapi.repository.HabitacionRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class HabitacionServices {
     private final HabitacionRepository habitacionRepository; // REPOSITORIO HABITACION
@@ -12,6 +14,14 @@ public class HabitacionServices {
     public HabitacionServices(HabitacionRepository habitacionRepository) {
         this.habitacionRepository = habitacionRepository;
     } // CONSTRUCTOR PARA AÑADIR EL REPOSITORIO HABITACION EN EL SERVICIO
+
+    public List<Habitacion> findAll() {
+        return habitacionRepository.findAll();
+    }
+
+    public void deleteById(Integer integer) {
+        habitacionRepository.deleteById(integer);
+    }
 
     public <S extends Habitacion> S save(S entity) {
         return habitacionRepository.save(entity);

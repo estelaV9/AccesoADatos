5daf33504b9d50210d824fa43ef24e4b09340fe1
estelaV9 <esteladeveg@gmail.com>
2025-01-15 12:53:48 +0100

    crear la clase usuario con sus atributos

---
+++ Tema5_API/Practica_APIyOpenAPI_Swagger/EstelaDeVega_PracticaAPI/src/main/java/com/example/esteladevega_practicaapi/model/User.java
@@ -0,0 +1,26 @@
+package com.example.esteladevega_practicaapi.model;
+
+public class User {
+    private String id;
+    private String password;
+    private String token;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+}

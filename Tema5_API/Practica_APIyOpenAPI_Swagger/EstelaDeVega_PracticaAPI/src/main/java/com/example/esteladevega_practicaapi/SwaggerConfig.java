780d123cf17ac33d8fae68b694d7edc41a079416
estelaV9 <esteladeveg@gmail.com>
2025-01-15 14:08:43 +0100

    crear la clase SwaggerConfig (el acceso sale denegado)

---
+++ Tema5_API/Practica_APIyOpenAPI_Swagger/EstelaDeVega_PracticaAPI/src/main/java/com/example/esteladevega_practicaapi/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.example.esteladevega_practicaapi;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("HotelesAPI")
+                        .description("API de Hoteles y Habitaciones")
+                        .contact(new Contact().name("juan").email("a@a.com").url("a.com"))
+                        .version("1.0"));
+    }
+}

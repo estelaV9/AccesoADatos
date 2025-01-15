6fa741deb80a0eac9314fd15a199fbe9ddece823
estelaV9 <esteladeveg@gmail.com>
2025-01-15 12:09:36 +0100

    modificar el main y ponerle WebSecurityConfigurerAdapter, añadimos las rutas que pueden o no ser accedidas por cualquier usuario

--- Tema5_API/Practica_APIyOpenAPI_Swagger/EstelaDeVega_PracticaAPI/src/main/java/com/example/esteladevega_practicaapi/App.java
+++ Tema5_API/Practica_APIyOpenAPI_Swagger/EstelaDeVega_PracticaAPI/src/main/java/com/example/esteladevega_practicaapi/App.java
@@ -1,13 +1,46 @@
 package com.example.esteladevega_practicaapi;
 
+import com.example.esteladevega_practicaapi.security.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+    @EnableWebSecurity
+    @Configuration
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .authorizeRequests()
+                    // PERMITE QUE LA BUSQUEDA DE HOTEL POR LOCALIDAD O CATEGORIA Y LA BUSQUEDA
+                    // DE UN HOTEL POR TAMAÑO Y PRECIO NO REQUIERAN NINGUN AUTENTIFICACION
+                    .antMatchers(HttpMethod.GET, "/api/hotel/localidad").permitAll()
+                    .antMatchers(HttpMethod.GET, "/api/hotel/categoria").permitAll()
+                    .antMatchers(HttpMethod.GET, "/api/habitacion/habitacionesLibres/{idHotel}/{tamanioMin}/{tamanioMax}/{precioMin}/{precioMax}").permitAll()
+
+                    // REQUIERE AUTORIZACION
+                    .antMatchers(HttpMethod.POST, "/api/habitacion/update/{idHabitacion}").authenticated()
+                    .antMatchers(HttpMethod.POST, "/api/habitacion/save").authenticated()
+                    .antMatchers(HttpMethod.GET, "/api/habitacion/all").authenticated()
+                    .antMatchers(HttpMethod.DELETE, "/api/habitacion/delete/{idHabitacion}").authenticated()
+
+                    .antMatchers(HttpMethod.POST, "/api/hotel/save").authenticated()
+                    .antMatchers(HttpMethod.GET, "/api/hotel/all").authenticated()
+                    // CUALQUIER SOLICITUD DEBE SER AUTENT  ICADA, DE LO CONTRARIO DEVOLVERA UNA RESPUESTA 401
+                    .anyRequest().authenticated();
+        }
+    }
 }
